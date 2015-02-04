f7de331c841d739b43f4efeb3ccb81640324223c
David Nadri <davidnadri@rk.local>
2015-02-04 18:59:03 -0500

    Initial Commit

---
+++ David101/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  David101
+//
+//  Created by David Nadri on 2/4/15.
+//  Copyright (c) 2015 David Nadri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
