fa5a8e304d6ac3e0b19e9e2457ff21d2f87e0d86
David Nadri <davidnadri@gmail.com>
2015-02-16 18:15:28 -0500

    Laying out imageview subclass

---
+++ David101/DNImageViewController.h
@@ -0,0 +1,18 @@
+//
+//  DNImageViewController.h
+//  David101
+//
+//  Created by David Nadri on 2/16/15.
+//  Copyright (c) 2015 David Nadri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DNImageViewController : UIImageView {
+    
+}
+
+//DVDavidPageType or DVPageType?
+- (id)initWithType:(DVPageType *) pageType;
+
+@end
---
+++ David101/DNImageViewController.m
@@ -0,0 +1,26 @@
+//
+//  DNImageViewController.m
+//  David101
+//
+//  Created by David Nadri on 2/16/15.
+//  Copyright (c) 2015 David Nadri. All rights reserved.
+//
+
+#import "DNImageViewController.h"
+#import "MainViewController.h"
+
+@implementation DNImageViewController
+
+// instancetype or id return type?
+// DVDavidPageType or DVPageType?
+- (instancetype)initWithType:(DVPageType *) pageType {
+    //I assume I get the URL from imageURLforType...
+    //then add asynchronous image loading code...?
+}
+
+// should this class method have a NSURL return type?
++ (NSURL) imageURLforType:(DVPageType *) pageType {
+    
+}
+
+@end
